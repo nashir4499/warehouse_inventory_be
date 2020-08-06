efc2dfdb00a80bd74d4cf58c584289f99ad7f178
M Nashir Mudzakir <mnashirmudzakir@gmail.com>
2020-08-06 09:21:55 +0700

    Membuat menu login yang masih umum

---
+++ database/seeds/UserSeeder.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const User = use('App/Models/User')
+const Factory = use('Factory')
+
+class UserSeeder {
+  async run() {
+    const u1 = new User()
+    u1.username = 'user1'
+    u1.password = 'pass-u1'
+    u1.email = 'u1@gmail.com'
+    await u1.save()
+
+    const u2 = new User()
+    u2.username = 'user2'
+    u2.password = 'pass-u2'
+    u2.email = 'u2@gmail.com'
+    await u2.save()
+
+  }
+}
+
+module.exports = UserSeeder
---
+++ start/routes.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('index')
+
+Route.get('profile', 'AuthController.getProfile').as('profile').middleware(['auth'])
+Route.get('login', 'AuthController.getLogin').as('login')
+Route.post('login', 'AuthController.postLogin').as('login')
+Route.post('logout', 'AuthController.postLogout').as('logout').middleware(['auth'])
+
+//menggunakan JWT
+Route.post('api/jwt/login', 'AuthController.postLoginJwt').as('loginJwt')
+Route.post('api/jwt/refresh', 'AuthController.postRefreshTokenJwt').as('refreshTokenJwt').middleware(['auth:jwt'])
+Route.post('api/jwt/logout', 'AuthController.postLogoutJwt').as('loginJwt').middleware(['auth:jwt'])
+Route.get('api/jwt/profile', 'AuthController.getProfileJwt').as('profileJwt').middleware(['auth:jwt'])
+
+//menggunakan API
+Route.post('api/api/login', 'AuthController.postLoginApi').as('loginApi')
+Route.post('api/api/logout', 'AuthController.postLogoutApi').as('logoutApi').middleware(['auth:api'])
+Route.post('api/api/logoutAll', 'AuthController.postLogoutApiAll').as('logoutApiAll').middleware(['auth:api'])
+Route.get('api/api/profile', 'AuthController.getProfileApi').as('profileApi').middleware(['auth:api'])
+
